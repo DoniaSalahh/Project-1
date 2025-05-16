35856ef0639ecc85f15b00dcaef38a88209796bc
DoniaSalahh <doniasalah12345678910@gmail.com>
2025-05-16 19:09:11 +0300

    Fix admin email in Login component

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import AcademyLogo from "../Assets/Outlook-4n2yii3h (1).gif";
 const Login = () => {
   // Admin credentials
   const ADMIN_CREDENTIALS = {
-    email: "Admin@vision.com",
+    email: "admin123@vision.com",
     password: "admin123",
   };
 
